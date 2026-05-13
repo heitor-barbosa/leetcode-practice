875f57fe4a8c5963fca015f9c94b6ee34c382aa0
Heitor Barbosa Souza <heitorbsouza20@gmail.com>
2026-05-13 14:55:22 -0300

    Add problem solution and description for 'Greatest Common Divisor of Strings'

---
+++ problems/strings/Greatest Common Divisor of Strings/solution.py
@@ -0,0 +1,31 @@
+# First Submission - O(n+m)
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        if len(str1) > len(str2):
+            biggest_str = str1
+            smallest_str = str2
+        else:
+            biggest_str = str2
+            smallest_str = str1
+
+        for i in range(len(smallest_str) + 1, -1, -1):
+            substr = smallest_str[:i]
+
+            if not (biggest_str.replace(substr, '')) and not (smallest_str.replace(substr, '')):
+                return substr
+        
+        return ''
+
+
+# Second / Optimal Submission - O(n+m)
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        if str1 + str2 != str2 + str1:
+            return ''
+
+        import math
+        gcd = math.gcd(len(str1), len(str2))
+
+        return str1[:gcd]
